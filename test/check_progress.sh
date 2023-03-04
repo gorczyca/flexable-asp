5de935cd6ac0e2a1f74b561d789c1ba56debab8d
gorczyca <gorczycapj@gmail.com>
2023-03-04 20:21:29 +0100

    Add experiments scripts and results

---
+++ test/check_progress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $1
+
+TOT_CORR=$(cat $1 | grep 'corr' | wc -l)
+TOT_INC=$(cat $1 | grep 'inc' | wc -l)
+TOT_TIMEOUT=$(cat $1 | grep 'TIMEOUT' | wc -l)
+TOT=$(cat $1 | wc -l)
+
+
+echo "TOTAL: $(($TOT-1))"
+echo "CORR.: $(($TOT_CORR-1))"
+echo "INC. : $TOT_INC"
+echo "TIME.: $TOT_TIMEOUT"
+
