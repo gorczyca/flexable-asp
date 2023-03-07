67146316f941746978a687078a0101b4d92f6f14
gorczyca <gorczycapj@gmail.com>
2023-03-07 09:39:48 +0100

    Add debug messages

--- multi_shot/flexaspMS.py
+++ multi_shot/flexaspMS.py
@@ -130,34 +130,44 @@ def get_flex_asp_answer(instance, goal, timeout):
     res = None
 
     while True:
+        print(f'step: {step}')
+        print('grounding updateState')
         ctrl.ground('updateState', step)
+        print('grounded updateState')
         # ctrl.ground('checkGameOn', step)
         # ctrl.assign_external('gameOn', step)
         # res = ctrl.solve(on_model=lambda model: on_model(step, model))
         # with ctrl.solve(on_model=lambda model: on_model(step, model), async_=True) as handle:
         with ctrl.solve(async_=True) as handle:
+            print('solving 1')
             while not handle.wait(1.0):
                 time_elapsed = time.time() - start_time
                 if time_elapsed > timeout:
+                    print('solving 1 timeout')
                     handle.cancel()
                     return None, timeout
-
+            print('solved 1')
             res = handle.get()
 
         if res.satisfiable: # <- game is still satisfiable
+            print('grounding check')
             ctrl.ground('check', step)
             ctrl.assign_external('query', step)
+            print('grounded check')
             # res = ctrl.solve(on_model=lambda model: on_model(step, model))
             # res = ctrl.solve()
 
             with ctrl.solve(async_=True) as handle:
+                print('solving 2')
                 while not handle.wait(1.0):
                     time_elapsed = time.time() - start_time
                     if time_elapsed > timeout:
+                        print('solving 2 timeout')
                         handle.cancel()
                         return None, timeout
             res = handle.get()
 
+            print('solved 2')
             if res.satisfiable:
                 return_value = 'yes'
                 break
@@ -165,8 +175,12 @@ def get_flex_asp_answer(instance, goal, timeout):
                 # ctrl.release_external('gameOn', step)
                 ctrl.release_external('query', step)
                 ctrl.cleanup()
+                print('cleaned-up')
+
                 step += 1
+                print('grounding next step')
                 ctrl.ground('step', step)
+                print('grounded next step')
         else:
             return_value = 'no'
             break
