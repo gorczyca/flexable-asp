0c445db6414f3d97d35d3070dd0a4e112db927f7
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2025-02-20 14:34:39 +0100

    Add test data.

--- new/config.py
+++ new/config.py
@@ -19,7 +19,8 @@ class Settings:
 
         self.python_path = '/home/piotr/anaconda3/envs/flexable/bin/python'
         self.aspforaba_results_path = '/home/piotr/Dresden/multishot/flexable-asp/test/aspforaba_results.csv'
-        self.output_path = 'results'
         self.instances_path = '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/aba-experiments-new/instances/asp_for_aba_instances'
+        
+        self.output_path = 'results'
         self.timeout = 600
 
