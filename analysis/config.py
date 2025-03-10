30b24e6b3ca3ee993a26568e424c09c8fc4efeb6
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2025-03-10 15:25:30 +0100

    analysis changes

--- analysis/config.py
+++ analysis/config.py
@@ -8,30 +8,49 @@ APPROACHES = {
     'aspforaba': Approach('aspforaba', '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/flexable_asp/repo/analysis/outputs/aspforaba_outputs.csv'),
     # 'abagraph': Approach('abagraph',''),
     'flexable': Approach('flexable', '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/flexable_asp/repo/flexable/flexable.csv'),
-    'assumptions+con': Approach('assumptions+constraints', '/home/piotr/Dresden/multishot/results-bak/assumptions_constr.csv'),
-    'externals+con': Approach('externals+constraints', '/home/piotr/Dresden/multishot/results-bak/externals_constr.csv'),
-    'assumptions': Approach('assumptions', '/home/piotr/Dresden/multishot/results-bak/assumptions_noconstr.csv'),
-    'externals': Approach('externals', '/home/piotr/Dresden/multishot/results-bak/externals_noconstr.csv')
-    # 'FAKEflexASPNew': Approach('FAKEflexASPNew', '/home/piotr/Dresden/multishot/flexable-asp/multi_shot_alt_test/multi_shot_alt_test.csv'),
-    # 'flexASPNew': Approach('flexASPNew', '/home/piotr/Dresden/multishot/flexable-asp/multi_shot_constraints/multi_shot_constraints.csv'),
-    # 'flexASP': Approach('flexASP', '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/flexable_asp/repo/multi_shot/multi_shot_1.csv'),
-    # 'flexASP1': Approach('flexable',''),
-    # 'flexASP2': Approach('flexable',''),
-    # 'flexASPAlt': Approach('flexable',''),
-    # 'flexASPAlt1': Approach('flexASPAlt1', '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/flexable_asp/repo/multi_shot_alt/multi_shot_alt.csv'),
-    # 'flexASPAlt2': Approach('flexASPAlt2', '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/flexable_asp/repo/multi_shot_alt_full_strategy/multi_shot_alt_strat.csv'),
+    'assumptions+con': Approach('assumptions+constraints', '/home/piotr/Dresden/multishot/results-assumptions-externals-constraints/assumptions_constr.csv'),
+    'externals+con': Approach('externals+constraints', '/home/piotr/Dresden/multishot/results-assumptions-externals-constraints/externals_constr.csv'),
+    'assumptions': Approach('assumptions', '/home/piotr/Dresden/multishot/results-assumptions-externals-constraints/assumptions_noconstr.csv'),
+    'externals': Approach('externals', '/home/piotr/Dresden/multishot/results-assumptions-externals-constraints/externals_noconstr.csv'),
+    # 
+    # 'single-5-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=5_s=True.csv'),
+    'single-5': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=5_s=False.csv'),
+    # 'single-10-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=10_s=True.csv'),
+    'single-10': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=10_s=False.csv'),
+    # 'single-15-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=15_s=True.csv'),
+    'single-15': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=15_s=False.csv'),
+    # 'single-20-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=20_s=True.csv'),
+    'single-20': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=20_s=False.csv'),
+    # 'single-25-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=25_s=True.csv'),
+    'single-25': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=25_s=False.csv'),
+    # 'single-30-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=30_s=True.csv'),
+    'single-30': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=30_s=False.csv'),
+    # 'single-50-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=50_s=True.csv'),
+    'single-50': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=50_s=False.csv'),
+    # 'single-75-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=75_s=True.csv'),
+    'single-75': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=75_s=False.csv'),
+    # 'single-100-s': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=100_s=True.csv'),
+    'single-100': Approach('externals', '/home/piotr/Dresden/multishot/results-singleshot/a=singleshot_c=False_x=100_s=False.csv'),
 }
 
 COLORS = [
-    '#6EB5FF',
-    '#612700',
-    '#B28DFF',
-    '#AFF8D8',
-    # '#612700',
-    '#FFB5E8',
-    '#DCD3FF',
-    # 'black',
+    "#1f77b4",  # Blue
+    "#ff7f0e",  # Orange
+    "#2ca02c",  # Green
+    "#d62728",  # Red
+    "#9467bd",  # Purple
+    "#8c564b",  # Brown
+    "#e377c2",  # Pink
+    "#7f7f7f",  # Gray
+    "#bcbd22",  # Yellow-Green
+    "#17becf",  # Cyan
+    "#ff5733",  # Deep Orange
+    "#5e3c99",  # Dark Purple
+    "#1a85ff",  # Vivid Blue
+    "#d41159",  # Magenta-Red
+    "#44aa99",  # Teal
 ]
+
 
 MARKERS = [
     '1',
--- analysis/table_analysis.py
+++ analysis/table_analysis.py
@@ -5,6 +5,7 @@ import utilities as ut
 
 
 OUTPUT_PATH = '/home/piotr/test/newest_ubuntu_data/Dresden/flexABle/flexable_asp/repo/analysis/comparison.csv'
+OUTPUT_PATH_TABLE = '/home/piotr/Dresden/multishot/flexable-asp/analysis/table.md'
 
 NON_TRIVIAL_INSTANCES_PATH = '/home/piotr/Dresden/multishot/flexable-asp/remove_trivial/instances_with_trivial.csv'
 
@@ -19,10 +20,13 @@ def get_data(df):
         df = df[nt['is_trivial'] == 'no']
         pass
 
+    total = len(df)
 
     timeouts = len(df[df['verdict'] == 'TIMEOUT'])
     solved = len(df) - timeouts
     incorrect = len(df[df['verdict'] == 'inc'])
+    accuracy = ((solved - incorrect) / solved) * 100 # in percent
+    accuracy_t = ((total - incorrect - timeouts) / total) * 100 # in percent
     time_total = df.duration.sum() / 3600
     idx_95_percentile = int(len(df) * .95)
     time_95 = df.sort_values(by='duration').iloc[:idx_95_percentile].duration.sum() / 3600
@@ -35,7 +39,7 @@ def get_data(df):
     max_ = not_timed_out.duration.max()
 
     # return [timeouts, time_total, time_95, min_, median_, mean_, max_]
-    return ['{:.2f}'.format(float(num)) for num in [solved, timeouts, incorrect, time_total, time_95, min_, median_, mean_, max_]]
+    return ['{:.2f}'.format(float(num)) for num in [solved, timeouts, incorrect, accuracy, accuracy_t, time_total, time_95, min_, median_, mean_, max_]]
     
 
 
@@ -43,9 +47,10 @@ if __name__ == '__main__':
     solvers_dfs = {approach: get_data(pd.read_csv(
         approach_obj.results_path)) for approach, approach_obj in cfg.APPROACHES.items()}
     df = pd.DataFrame(solvers_dfs)
-    df.index = ['solved', 'timeouts', 'incorrect', 'time_tot [h]', 'time_95 [h]', 'min [s]', 'median [s]', 'mean [s]', 'max [s]' ]
+    df.index = ['solved', 'timeouts', 'incorrect', 'accuracy [%]', 'accuracy t [%]', 'time_tot [h]', 'time_95 [h]', 'min [s]', 'median [s]', 'mean [s]', 'max [s]' ]
     df.to_csv(OUTPUT_PATH)
     print('Without' if FILTER_TRIVIAL else 'With', 'trivial instances')
     print(df.to_markdown())
+    df.to_markdown(OUTPUT_PATH_TABLE)
     
     
